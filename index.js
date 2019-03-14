cbc140e5ea671d8685eab4110d973a157cb2df5f
harmonious-innovator-3896 <kevin20130209@hotmail.com>
2019-03-14 18:18:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name)
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
+
+function nowServing(deliLine) {
+  if (deliLine.length === 0) {
+    return 'There is nobody waiting to be served!'
+  } else {
+    return `Currently serving ${deliLine.shift()}.`
+  }
+}
