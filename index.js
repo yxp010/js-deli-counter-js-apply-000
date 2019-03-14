7628b8a4de6b3af043755b1d56f627e8fc82e52f
harmonious-innovator-3896 <kevin20130209@hotmail.com>
2019-03-14 18:31:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-
-
-var katzDeliLine = []
-
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name)
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
@@ -12,5 +8,24 @@ function nowServing(deliLine) {
     return 'There is nobody waiting to be served!'
   } else {
     return `Currently serving ${deliLine.shift()}.`
+  }
+}
+
+function currentLine(line) {
+  if (line.length === 0) {
+    return "The line is currently empty."
+  } else {
+    var waitingLine = "The line is currently: "
+    var i = 0
+    var len = line.length
+
+    for (;i < len; i++) {
+      if (i > 0) {
+        `${waitingLine} + , + ${i + 1} + ' ' + ${line[i]}`
+      } else {
+        `${waitingLine} + ${i + 1} + ' ' + ${line[i]}`
+      }
+    }
+    return waitingLine
   }
 }
